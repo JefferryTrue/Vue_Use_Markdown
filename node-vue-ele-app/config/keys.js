6313eaee9874364b5ec1536bc15632fe19f93011
Jefferry <1624410543@qq.com>
2022-08-11 23:59:26 +0800

    solved bugs

--- node-vue-ele-app/config/keys.js
+++
@@ -1,6 +0,0 @@
-const { model } = require("mongoose");
-
-module.exports = {
-    mongoURI:"mongodb+srv://JefferryTrue:qQ13527503399@restful-api-prod.bzsty4w.mongodb.net/?retryWrites=true&w=majority",
-    secretOrKey:"secret"
-}
